c516f0ceb848774da8dee2e003ce06abca472e21
Tasik1 <106980028+Tasik1@users.noreply.github.com>
2022-06-11 21:15:03 +0600

    Add files via upload

---
+++ font-awesome.js
@@ -0,0 +1,100 @@
+const track = document.getElementById("track");
+const trackArtist = document.getElementById("track-artist");
+const trackTitle = document.getElementById("track-title");
+const progressBar = document.getElementById("progressBar");
+const currentTime = document.getElementById("currentTime");
+const durationTime = document.getElementById("durationTime");
+const video = document.getElementById("background")
+
+let play = document.getElementById("play");
+let pause = document.getElementById("pause");
+let next = document.getElementById("next-track");
+let prev = document.getElementById("prev-track");
+trackIndex = 0;
+
+tracks = [
+  "rengokuTheme.mp3",
+  "akazaTheme.mp3",
+  "Harry Styles - As It Was.mp3"
+];
+
+trackArtists = ["Samuel Kim", "Samuel Kim", "Harry Styles"];
+trackTitles = ["Rengoku Theme", "Akaza Theme", "As it was"];
+
+let playing = true;
+function pausePlay() {
+  if (playing) {
+    play.className="far fa-pause-circle fa-3x";
+    track.play();
+    video.play();
+    playing = false;
+  } else {
+    play.className="far fa-play-circle fa-3x";
+    video.pause();
+    track.pause();
+    playing = true;
+  }
+}
+
+play.addEventListener("click", pausePlay);
+
+track.addEventListener("ended", nextTrack);
+
+function nextTrack() {
+  trackIndex++;
+  if (trackIndex > tracks.length - 1) {
+    trackIndex = 0;
+  }
+  video.currentTime = "0"
+  track.src = tracks[trackIndex];
+
+  trackArtist.textContent = trackArtists[trackIndex];
+  trackTitle.textContent = trackTitles[trackIndex];
+
+  playing = true;
+  pausePlay();
+}
+
+next.addEventListener("click", nextTrack);
+
+function prevTrack() {
+  trackIndex--;
+  if (trackIndex < 0) {
+    trackIndex = tracks.length - 1;
+  }
+
+  track.src = tracks[trackIndex];
+
+  trackArtist.textContent = trackArtists[trackIndex];
+  trackTitle.textContent = trackTitles[trackIndex];
+
+  playing = true;
+  pausePlay();
+}
+
+prev.addEventListener("click", prevTrack);
+
+function progressValue() {
+  progressBar.max = track.duration;
+  progressBar.value = track.currentTime;
+
+  currentTime.textContent = formatTime(track.currentTime);
+  durationTime.textContent = formatTime(track.duration);
+}
+
+setInterval(progressValue, 500);
+
+function formatTime(sec) {
+  let minutes = Math.floor(sec / 60);
+  let seconds = Math.floor(sec - minutes * 60);
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${minutes}:${seconds}`;
+}
+
+function changeProgressBar() {
+  track.currentTime = progressBar.value;
+}
+
+progressBar.addEventListener("click", changeProgressBar);
